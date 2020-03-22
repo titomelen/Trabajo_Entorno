107cb9d6801cd480fa9b2d8ccbd5e96b1cada657
carlos <56443204+carlos-ada@users.noreply.github.com>
2020-03-22 22:30:39 +0100

    Arreglado Test Funcion 4 Carlos Villar

---
+++ PruebaFuncion4.java
@@ -0,0 +1,50 @@
+package Funciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PruebaFuncion4 {
+
+    static Funcion3 funcion = null;
+    static int pruebasRealizadas = 0;
+
+    @BeforeAll
+    static void prepararPruebas() {
+        System.out.println("Preparando Tests");
+        funcion = new Funcion3();
+    }
+
+    @BeforeEach
+    private void preparaPruebaEjecutar() {
+        pruebasRealizadas++;
+        System.out.println("Ejecutando test " + pruebasRealizadas);
+    }
+
+    @AfterEach
+    private void terminadaPrueba() {
+        System.out.println("Se ha ejecutado el test: " + pruebasRealizadas);
+    }
+
+    @AfterAll
+    static void terminarPruebas() {
+        System.out.println("Se ha terminado de ejecutar todas las pruebas");
+        funcion = null;
+    }
+    @DisplayName("Carlos Villar")
+    @Test
+
+    void pruebaFuncion4_3(){
+
+        String [] nombres = {"jut", "car", "Pep"};
+
+        assertEquals(nombres[(int) Math.floor(Math.random()*nombres.length)] ,funcion.Funcion4 ("javier", "alfonso", "pepe"));
+
+    }
+}
