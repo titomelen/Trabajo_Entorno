7e8ee1a2b79be8223f9ef6c5e042ef1680d0bb43
titomelen <56442515+titomelen@users.noreply.github.com>
2020-03-22 23:08:06 +0100

    #28 Voy a subir el test de la función 3

---
+++ PruebaFuncion3(Javier).java
@@ -0,0 +1,45 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PruebaFuncion3(Javier) {
+
+    static Funcion3 funcion = null;
+    static int pruebasRealizadas = 0;
+
+    @BeforeAll
+    static void prepararPruebas() {
+        System.out.println("Preparando Tests");
+        funcion = new Funcion3();
+    }
+
+    @BeforeEach
+    private void preparaPruebaEjecutar() {
+        pruebasRealizadas++;
+        System.out.println("Ejecutando test " + pruebasRealizadas);
+    }
+
+    @AfterEach
+    private void terminadaPrueba() {
+        System.out.println("Se ha ejecutado el test: " + pruebasRealizadas);
+    }
+
+    @AfterAll
+    static void terminarPruebas() {
+        System.out.println("Se ha terminado de ejecutar todas las pruebas");
+        funcion = null;
+    }
+
+    @DisplayName("Test de Caja Negra sin valor")
+    @Test
+    void testFuncion3_3() {
+        assertEquals(0, Funcion.Funcion3());
+    }
+}
