47dbc1c4e98e73e743988729a695f2636d6c766a
Juan2707pog <56446340+Juan2707pog@users.noreply.github.com>
2020-03-22 23:26:55 +0100

    Add files via upload

---
+++ PruebaFuncion7(LIMON).java
@@ -0,0 +1,89 @@
+package Funciones;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+import org.junit.jupiter.api.AfterAll;
+
+import org.junit.jupiter.api.AfterEach;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.DisplayName;
+
+import org.junit.jupiter.api.Test;
+
+
+
+public class Prueba7 {
+
+
+
+    static Funciones funcion = null;
+
+    static int pruebasRealizadas = 0;
+
+
+
+    @BeforeAll
+
+    static void prepararPruebas() {
+
+        System.out.println("Iniciando Pruebas");
+
+        funcion = new Funcion();
+
+    }
+
+
+
+    @BeforeEach
+
+    private void preparaPruebaEjecutar() {
+
+        pruebasRealizadas++;
+
+        System.out.println("Pruebas realizandose " + pruebasRealizadas);
+
+    }
+
+
+
+    @AfterEach
+
+    private void terminadaPrueba() {
+
+        System.out.println("Pruebas realizadas: " + pruebasRealizadas);
+
+    }
+
+
+
+    @AfterAll
+
+    static void terminarPruebas() {
+
+        System.out.println("Final de pruebas");
+
+        funcion = null;
+
+    }
+
+
+
+    @DisplayName("Test de Caja Negra con array-limon")
+
+    @Test
+
+    void testFuncion7() {
+
+        assertArrayEquals({"ocle", "isti", "onof", "ill"},
+
+                funcion.funcion7({"Esternocleidomastoideo", "Futbolistico", "Soyunmonofeliz", "Pentakill"}));
+
+    }
