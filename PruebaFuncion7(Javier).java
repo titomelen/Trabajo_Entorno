1f797e30d481092d195b677df6978d145e59369b
titomelen <56442515+titomelen@users.noreply.github.com>
2020-03-22 23:48:27 +0100

    #35 Subo el test de la función 7

---
+++ PruebaFuncion7(Javier).java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PruebaFuncion7(Javier) {
+
+    static Funcion7 funcion = null;
+    static int pruebasRealizadas = 0;
+
+    @BeforeAll
+    static void prepararPruebas() {
+        System.out.println("Preparando Tests");
+        funcion = new Funcion7();
+    }
+
+    @BeforeEach
+    private void preparaPruebaEjecutar() {
+        pruebasRealizadas++;
+        System.out.println("Ejecutando test " + pruebasRealizadas);
+    }
+
+    @AfterEach
+    private void terminadaPrueba() {
+        System.out.println("Se ha ejecutado el test: " + pruebasRealizadas);
+    }
+
+    @AfterAll
+    static void terminarPruebas() {
+        System.out.println("Se ha terminado de ejecutar todas las pruebas");
+        funcion = null;
+    }
+
+    @DisplayName("Test de analisis de limites")
+    @Test
+    void testFuncion7_2() {
+        assertArrayEquals("","","","", Funcion7.Funcion7("a", "b", "c","d"));
+        assertArrayEquals("123456", Funcion7.Funcion7("14534657764"));
+    }
+}
