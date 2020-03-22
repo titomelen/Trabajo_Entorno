d8ffa668e44f2b9eb8b2fd1d8e2534a67a1a7f3d
carlos <56443204+carlos-ada@users.noreply.github.com>
2020-03-22 21:46:45 +0100

    #16 Prueba Test 6 Carlos Villar

---
+++ PruebaFuncion6.java
@@ -0,0 +1,47 @@
+package Funciones;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PruebaFuncion6 {
+
+    static Funcion3 funcion = null;
+    static int pruebasRealizadas = 0;
+
+    @BeforeAll
+    static void prepararPruebas() {
+        System.out.println("Preparando Tests");
+        funcion = new Funcion3();
+    }
+
+    @BeforeEach
+    private void preparaPruebaEjecutar() {
+        pruebasRealizadas++;
+        System.out.println("Ejecutando test " + pruebasRealizadas);
+    }
+
+    @AfterEach
+    private void terminadaPrueba() {
+        System.out.println("Se ha ejecutado el test: " + pruebasRealizadas);
+    }
+
+    @AfterAll
+    static void terminarPruebas() {
+        System.out.println("Se ha terminado de ejecutar todas las pruebas");
+        funcion = null;
+    }
+    @DisplayName("Carlos Villar")
+    @Test
+    void testFuncion6_2() {
+        assertNull(0, 0, 0, 0, funcion.Funcion6(null));
+    } 
+}
+
